97a5b1825d05fd31ba5c249b8cae0a4e781621bd
Ricardo Airton Silva <ricardoairtonsilva@hotmail.com>
2023-05-14 08:52:14 +0100

    updated demo script

--- demo.sh
+++ demo.sh
@@ -1,48 +1,47 @@
-//todo mudar localhost para l1 e l2
 source packages/contracts-bedrock/.env
 
-////1st step
+# 1st step
+# Initial State Viewing
 cast balance $TESTER_ADDR --rpc-url $L1
 cast call $ERC20 "balanceOf(address)(uint256)" $TESTER_ADDR --rpc-url $L1
 cast balance $TESTER_ADDR --rpc-url $L2
 
 
-// send funds to tester
-//State changin
+# Send gas funds to tester, approve on erc20 and execute bridging
+# State changing
 cast send $TESTER_ADDR --value 10ether --rpc-url $L1 --private-key $DEPLOYER_PK
 cast send $ERC20 "approve(address,uint256)" $PORTAL_L1 10000000000000000000000000000 --rpc-url $L1 --private-key $TESTER_PK
 cast send $PORTAL_L1 "depositTransaction(address,uint256,uint64,bool,bytes)" $TESTER_ADDR 10000000000000000000 1000000 false 0x00 --private-key $TESTER_PK
 
-//view results
+# View results
 cast call $ERC20 "balanceOf(address)(uint256)" $TESTER_ADDR --rpc-url $L1
 cast balance $TESTER_ADDR --rpc-url $L1
 cast balance $TESTER_ADDR --rpc-url $L2
 
 
-//2nd step
-//state change
-//verify trasnfers in l2
+# 2nd step
+# State change
+# Verify tester2 balance
 cast balance $TESTER_ADDR2 --rpc-url $L2
+# Send balance From tester1 to tester2
 cast send $TESTER_ADDR2 --value 5000000000000000000 --rpc-url $L2 --private-key $TESTER_PK
 
-//view results
+# View results
+# View balance of both users on L2
 cast balance $TESTER_ADDR --rpc-url $L2
 cast balance $TESTER_ADDR2 --rpc-url $L2
 
 
-//3rd step
+# 3rd step
+# Check Tester2's ERC balance on L1 and verify that it is 0
 cast call $ERC20 "balanceOf(address)(uint256)" $TESTER_ADDR2 --rpc-url $L1
-# cast send $TESTER_ADDR2 --value somevalue --private-key $TESTER_PK --rpc-url $L2
+
+
+#### BRIDGE BACK TO PORTAL L2
 cast send $PORTAL_L2 --value 4000000000000000000 --private-key $TESTER2_PK --rpc-url $L2
 
-//
-# cast balance $TESTER_ADDR --rpc-url $L2
+# View state changes and verify that tester2 No longer has L2 balance
+# And now has ERC20 balance instead
 cast balance $TESTER_ADDR2 --rpc-url $L2
 cast call $ERC20 "balanceOf(address)(uint256)" $TESTER_ADDR2 --rpc-url $L1
-# //todo set this portal
-# cast send $PORTAL_L2 --value asjkfbsajkbfgkajslbfga --private-key $TESTER_PK
 
-
-
-# cast balance $TESTER_ADDR2 --rpc-url $L2
-# cast call $ERC20 "balanceOf(address)(uint256)" $TESTER_ADDR2 --rpc-url $L1
